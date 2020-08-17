5169840a1f4023f56ea6abe858612ed26f07c295
nimishanimz <63094535+nimishanikz@users.noreply.github.com>
2020-08-17 20:48:12 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+import * as firebase from 'firebase'
+require('@firebase/firestore')
+ 
+
+var firebaseConfig = {
+    apiKey: "AIzaSyBjEGjaS-OCYWzzNqzeLAFdk-eFsJi1JGo",
+    authDomain: "wily-app-8a616.firebaseapp.com",
+    databaseURL: "https://wily-app-8a616.firebaseio.com",
+    projectId: "wily-app-8a616",
+    storageBucket: "wily-app-8a616.appspot.com",
+    messagingSenderId: "1055709847670",
+    appId: "1:1055709847670:web:48057530112d2abc26ff57"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  export default firebase.firestore();
